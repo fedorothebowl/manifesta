85ea2a53fb6028e4088194a1cdc2f1a780a915c2
federico petrucci <federico@quivi.it>
2024-06-01 17:11:47 +0200

    test fullscreen

--- main.js
+++ main.js
@@ -5,3 +5,16 @@ import Answer from './resources/js/Answer';
 
 Alpine.data('Answer', Answer);
 Alpine.start();
+
+
+window.addEventListener("load", ()=>{
+    if (document.documentElement.requestFullscreen) {
+        document.documentElement.requestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) { // Firefox
+        document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari and Opera
+        document.documentElement.webkitRequestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
+        document.documentElement.msRequestFullscreen();
+    }
+})
