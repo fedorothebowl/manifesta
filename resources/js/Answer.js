b89603dc266edc7aefde9a68bed25a4a9196b364
federico petrucci <federico@quivi.it>
2024-06-02 14:48:26 +0200

    avanzamento

--- resources/js/Answer.js
+++ resources/js/Answer.js
@@ -1,16 +1,18 @@
-import confetti from './confetti.js';
+import confetti from "./confetti.js";
 
 export default function () {
-
   return {
-
     data: [],
 
     step: 0,
     answers: [],
-    wrongAnswers:[],
+    wrongAnswers: [],
     score: 0,
-    showPopUp:false,
+    popupFinalStep: false,
+    checkAnswer: {
+      showPopup: false,
+      validateAnswer: undefined,
+    },
 
     answer: {
       question: "",
@@ -19,13 +21,13 @@ export default function () {
 
     async init() {
       await this.fetchJsonData();
-      this.$refs.stepper.style.width = (this.step) * 10 + "%";
+      this.$refs.stepper.style.width = this.step * 10 + "%";
       this.nextAnswer();
     },
 
     async fetchJsonData() {
       try {
-        let response = await fetch('/assets/json/question.json');
+        let response = await fetch("/assets/json/question.json");
 
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
@@ -45,50 +47,58 @@ export default function () {
     nextAnswer() {
       this.shuffleArray(this.data[this.step].answers); // mescolo le risposte
       this.answer = this.data[this.step];
+      this.$refs.image.src=this.answer.image;
     },
 
     nextStep() {
+      this.checkAnswer.showPopup = false;
       if (this.step < 9) {
         this.step++;
         this.nextAnswer();
-        this.$refs.stepper.style.width = (this.step) * 10 + "%";
-      }else{
+        this.$refs.stepper.style.width = this.step * 10 + "%";
+      } else {
         this.finalStep();
       }
     },
 
     handleScore(element) {
+      this.checkAnswer.showPopup = true;
       const value = element.querySelector("span").getAttribute("data-type");
-      value == "real" ? this.score++ : this.wrongData();
-      this.nextStep();
-    },
 
-    finalStep(){
-      this.$refs.stepper.style.width = 100 + "%";
-      setTimeout(() => {
-        this.step = 10;
-        this.showPopUp = true;
-        if(this.score >= 7){
-          this.winner();
-        }else{
-          this.loser(); 
+      this.answer.answers.forEach(el=>{
+        if (el.type == "real"){
+          console.log(el.answer)
+          this.$refs.test.innerHTML = el.answer;
         }
-      }, 1000);
+      });
+
+      if (value == "real") {
+        this.checkAnswer.validateAnswer = true;
+        this.score++;
+      } else {
+        this.checkAnswer.validateAnswer = false;
+      }
+
     },
 
-    loser(){
+    finalStep() {
+      this.step = 10;
+      this.$refs.stepper.style.width = 100 + "%";
+      this.popupFinalStep = true;
+      this.score >= 7 ? this.winner()  : this.loser();
+    },
+
+    loser() {
       this.$refs.loserSong.play();
     },
 
-    winner(){
+    winner() {
       confetti();
       this.$refs.winnerSong.play();
     },
 
-    wrongData(){
-      this.wrongAnswers.push(this.data[this.step])
-    }
-
+    // wrongData(){
+    //   this.wrongAnswers.push(this.data[this.step])
+    // }
   };
-
 }
